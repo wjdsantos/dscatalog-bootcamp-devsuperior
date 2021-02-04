7dd0e691954d6bd529be610a8a29897c3a881b39
wjdsantos <walter4asantos@gmail.com>
2021-02-04 16:34:21 -0300

    Dealing with forms in ReactJS

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
 const Form = () => {
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('');
+
+    const handdleOnChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+       setName(event.target.value);
+    }
+
+    const handdleOnChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPrice(event.target.value);
+     }
+
+     const handdleOnChangeCategory = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setCategory(event.target.value);
+     }
+
     return (
-        <BaseForm title="CADASTRAR UM PRODUTO">
+        <BaseForm title="cadastrar um produto">
+            <h1 className="my-5">
+               Nome: {name} <br />
+               preço: {price} <br />
+               Categoria: {category}
+            </h1>
             <div className="row">
                 <div className="col-6">
-                    <input type="text" className="form-control" />
+                    <input 
+                        value={name}
+                        type="text"
+                        className="form-control mb-5" 
+                        onChange={handdleOnChangeName}
+                        placeholder="Nome do produto"
+                    />
+                    <select value={category} className="form-control mb-5" onChange={handdleOnChangeCategory}>
+                        <option value="livros">Livros</option>
+                        <option value="computadores">Computadores</option>
+                        <option value="eletronicos">Eletrônicos</option>
+                    </select>
+                     <input 
+                        value={price}
+                        type="text"
+                        className="form-control" 
+                        onChange={handdleOnChangePrice}
+                        placeholder="Preço"
+                    />
                 </div>
             </div>
         </BaseForm>
