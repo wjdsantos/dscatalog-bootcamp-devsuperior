711e00eb1a3fb55c05a9c53ba32486678df18bf9
wjdsantos <walter4asantos@gmail.com>
2021-02-04 18:03:26 -0300

    Generic structure created to handle multiple form fields

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -2,54 +2,69 @@ import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
-const Form = () => {
-    const [name, setName] = useState('');
-    const [price, setPrice] = useState('');
-    const [category, setCategory] = useState('');
+type FormState = {
+    name: string;
+    price: string;
+    category: string;
+}
 
-    const handdleOnChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
-       setName(event.target.value);
+const Form = () => {
+    const [formData, setFormData] = useState<FormState>({
+        name: 'Computador',
+        price: '',
+        category: ''
+    });
+
+//    const [price, setPrice] = useState('');
+//    const [category, setCategory] = useState('');
+
+    const handdleOnChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setFormData(data => ({ ...data, [name]: value }));
     }
 
-    const handdleOnChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPrice(event.target.value);
-     }
-
-     const handdleOnChangeCategory = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setCategory(event.target.value);
-     }
-
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+ 
+        console.log(formData);
+    }
     return (
-        <BaseForm title="cadastrar um produto">
-            <h1 className="my-5">
-               Nome: {name} <br />
-               preço: {price} <br />
-               Categoria: {category}
-            </h1>
-            <div className="row">
-                <div className="col-6">
-                    <input 
-                        value={name}
-                        type="text"
-                        className="form-control mb-5" 
-                        onChange={handdleOnChangeName}
-                        placeholder="Nome do produto"
-                    />
-                    <select value={category} className="form-control mb-5" onChange={handdleOnChangeCategory}>
-                        <option value="livros">Livros</option>
-                        <option value="computadores">Computadores</option>
-                        <option value="eletronicos">Eletrônicos</option>
-                    </select>
-                     <input 
-                        value={price}
-                        type="text"
-                        className="form-control" 
-                        onChange={handdleOnChangePrice}
-                        placeholder="Preço"
-                    />
+        <form onSubmit={handleSubmit}>
+            <BaseForm title="cadastrar um produto">
+                <div className="row">
+                    <div className="col-6">
+                        <input 
+                            value={formData.name}
+                            name="name"
+                            type="text"
+                            className="form-control mb-5" 
+                            onChange={handdleOnChange}
+                            placeholder="Nome do produto"
+                        />
+                        <select 
+                            value={formData.category}
+                            className="form-control mb-5"
+                            onChange={handdleOnChange}
+                            name="category"
+                        >
+                            <option value="livros">Livros</option>
+                            <option value="computadores">Computadores</option>
+                            <option value="eletronicos">Eletrônicos</option>
+                        </select>
+                        <input 
+                            value={formData.price}
+                            name="price"
+                            type="text"
+                            className="form-control" 
+                            onChange={handdleOnChange}
+                            placeholder="Preço"
+                        />
+                    </div>
                 </div>
-            </div>
-        </BaseForm>
+            </BaseForm>
+        </form>
     )
 }
 
